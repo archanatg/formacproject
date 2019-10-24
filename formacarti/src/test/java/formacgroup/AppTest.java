53fd81f9a45dfb7be0545d35ed22ef8d20197fec
Deepak T V S <tvsdeepak@deepaks-mbp-2.lan>
2019-10-24 10:48:59 -0700

    formacarti added

---
+++ formacarti/src/test/java/formacgroup/AppTest.java
@@ -0,0 +1,20 @@
+package formacgroup;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
